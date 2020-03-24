1168d2545edb7454012ffb99e7b6c6f4b31a809e
fa-fujimoto <crossclose00+private@gmail.com>
2020-03-24 21:43:54 +0900

    #1 支援点の合計表示コンポーネントを追加。コンポーネントの追加に合わせて、ペア選択アイテムのデザインを修正。

---
+++ client/TotalPoint.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import { createClassName } from './Util'
+
+interface ITotalPointProps {
+  maxTotalPoint: number
+  currentTotalPoint: number
+}
+
+const TotalPoint: FC<ITotalPointProps> = ({maxTotalPoint, currentTotalPoint}) => {
+  return (
+    <dl className={createClassName('total-point')}>
+      <dt className={createClassName('total-point', 'label')}>合計点</dt>
+      <dd className={createClassName('total-point', 'value', maxTotalPoint === currentTotalPoint ? '' : 'invalid')}>
+        {`${currentTotalPoint}/${maxTotalPoint}`}
+      </dd>
+    </dl>
+  )
+}
+
+export default TotalPoint
